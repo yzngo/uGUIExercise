292898f1ffade6644ae67c819185633e7733a136
Zhenning Yang <yzngo@live.com>
2019-12-19 20:59:41 +0800

    add baum2

---
+++ Assets/Baum2/Sample/Sample.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Baum2.Sample
+{
+    public class Sample : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject uiPrefab;
+
+        private UIRoot ui;
+        private int listSize;
+
+        public void Start()
+        {
+            ui = BaumUI.Instantiate(gameObject, uiPrefab);
+            listSize = 10;
+            ImageSample();
+            ButtonSample();
+            ListSample();
+        }
+
+        public void Update()
+        {
+            SliderSample();
+        }
+
+        public void ImageSample()
+        {
+            ui.Get("Image1").SetActive(false);
+            ui.Get("Image2").SetActive(false);
+        }
+
+        public void ButtonSample()
+        {
+            var welcomeText = ui.Get<Text>("Welcome/Text");
+            var list = ui.Get<List>("PiyoList");
+
+            ui.Get<Button>("HogeButton").onClick.AddListener(() =>
+            {
+                welcomeText.text = "Welcome to Hoge!";
+                ui.Get("Image1").SetActive(true);
+                ui.Get("Image2").SetActive(false);
+                list.Resize(++listSize);
+            });
+
+            ui.Get<Button>("FugaButton").onClick.AddListener(() =>
+            {
+                welcomeText.text = "Welcome to Fuga!";
+                ui.Get("Image1").SetActive(false);
+                ui.Get("Image2").SetActive(true);
+                list.Resize(--listSize);
+            });
+        }
+
+        public void ListSample()
+        {
+            var list = ui.Get<List>("PiyoList");
+            list.Scrollbar = ui.Get<Scrollbar>("PiyoScrollbar");
+            list.LayoutGroup.Spacing = 10;
+            list.UISelector = (_) => "Item1";
+            list.UIUpdater = (listUI, index) =>
+            {
+                listUI.Get<Text>("ListItemText").text = string.Format("Piyo: {0}", index);
+
+                var button = listUI.Get<Button>("ItemButton", true);
+                if (button != null)
+                {
+                    button.onClick.AddListener(() => Debug.Log(index));
+                }
+            };
+            list.Resize(listSize);
+        }
+
+        public void SliderSample()
+        {
+            ui.Get<Slider>("HPSlider").value = Mathf.Clamp01(Time.time % 1.0f);
+        }
+    }
+}
---
+++ Assets/Baum2/Scripts/List.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// ReSharper disable InconsistentNaming
+
+namespace Baum2
+{
+    public class List : MonoBehaviour
+    {
+        [SerializeField]
+        public List<GameObject> ItemSources;
+
+        [SerializeField]
+        public ListLayoutGroup LayoutGroup;
+
+        private GameObject contentCache;
+        public GameObject Content
+        {
+            get
+            {
+                if (contentCache != null) return contentCache;
+                contentCache = gameObject.transform.Find("Content").gameObject;
+                return contentCache;
+            }
+        }
+
+        private RectTransform contentRectTransformCache;
+        public RectTransform ContentRectTransform
+        {
+            get
+            {
+                if (contentRectTransformCache != null) return contentRectTransformCache;
+                contentRectTransformCache = Content.GetComponent<RectTransform>();
+                return contentRectTransformCache;
+            }
+        }
+
+        private RectTransform rectTransformCache;
+        public RectTransform RectTransform
+        {
+            get
+            {
+                if (rectTransformCache != null) return rectTransformCache;
+                rectTransformCache = GetComponent<RectTransform>();
+                return rectTransformCache;
+            }
+        }
+
+        private ScrollRect scrollRectCache;
+        public ScrollRect ScrollRect
+        {
+            get
+            {
+                if (scrollRectCache != null) return scrollRectCache;
+                scrollRectCache = GetComponent<ScrollRect>();
+                return scrollRectCache;
+            }
+        }
+
+        public Scrollbar Scrollbar
+        {
+            set
+            {
+                if (LayoutGroup.Scroll == Scroll.Horizontal) ScrollRect.horizontalScrollbar = value;
+                else if (LayoutGroup.Scroll == Scroll.Vertical) ScrollRect.verticalScrollbar = value;
+                else throw new ApplicationException("LayoutGroup not found");
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return Items.Count;
+            }
+        }
+
+        private Func<int, string> uiSelector;
+        public Func<int, string> UISelector
+        {
+            get
+            {
+                return uiSelector;
+            }
+            set
+            {
+                uiSelector = value;
+                UpdateAll();
+            }
+        }
+
+        private Action<string, UIRoot> uiFactory;
+        public Action<string, UIRoot> UIFactory
+        {
+            get
+            {
+                return uiFactory;
+            }
+            set
+            {
+                uiFactory = value;
+            }
+        }
+
+        private Action<UIRoot, int> uiUpdater;
+        public Action<UIRoot, int> UIUpdater
+        {
+            get
+            {
+                return uiUpdater;
+            }
+            set
+            {
+                uiUpdater = value;
+                UpdateAll();
+            }
+        }
+
+        private readonly List<UIRoot> Items = new List<UIRoot>();
+
+        public void Awake()
+        {
+            foreach (var itemSource in ItemSources)
+            {
+                itemSource.SetActive(false);
+            }
+        }
+
+        private UIRoot AddItem(string sourceName)
+        {
+            var original = ItemSources.Find(x => x.name == sourceName);
+            var item = Instantiate(original, Content.transform, true);
+            item.transform.localPosition = original.transform.localPosition;
+            item.transform.localScale = Vector3.one;
+            item.SetActive(true);
+
+            var uiRoot = item.AddComponent<UIRoot>();
+            var cache = item.AddComponent<Cache>();
+            cache.CreateCache(item.transform);
+            uiRoot.Awake();
+
+            Items.Add(uiRoot);
+            LayoutGroup.RequestUpdate();
+
+            return uiRoot;
+        }
+
+        public UIRoot AddItemDirect(string sourceName)
+        {
+            return AddItem(sourceName);
+        }
+
+        public void Resize(int size)
+        {
+            foreach (Transform item in Content.transform)
+            {
+                Destroy(item.gameObject);
+            }
+            Items.Clear();
+
+            for (var i = 0; i < size; ++i)
+            {
+                var sourceName = uiSelector(i);
+                var item = AddItem(sourceName);
+                if (uiFactory != null) uiFactory(sourceName, item);
+                if (uiUpdater != null) uiUpdater(item, i);
+            }
+        }
+
+        public void UpdateItem(int index)
+        {
+            if (uiUpdater != null) uiUpdater(Items[index], index);
+        }
+
+        public void UpdateAll()
+        {
+            if (uiUpdater == null) return;
+            for (var i = 0; i < Items.Count; ++i)
+            {
+                uiUpdater(Items[i], i);
+            }
+        }
+
+        public void ResetScroll()
+        {
+            ScrollRect.velocity = Vector2.zero;
+            LayoutGroup.ResetScroll();
+        }
+
+        public void ScrollTo(int index)
+        {
+            ScrollRect.velocity = Vector2.zero;
+            LayoutGroup.ScrollToIndex(index);
+        }
+    }
+}
