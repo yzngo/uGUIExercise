3bc47dd562428823dc8fcdf6e3565e1255ef556a
Yzngo <yzngo@live.com>
2019-12-22 13:43:58 +0800

    update

---
+++ Assets/Lua/LoadLua.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System;
+
+public class LoadLua: MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    public static void Init()
+    {
+        LuaEnv luaenv = null;
+        // Use this for initialization
+        luaenv = new LuaEnv();
+        luaenv.DoString("require 'main'");
+        Action main = luaenv.Global.Get<Action>("main");//映射到一个delgate，要求delegate加到生成列表，否则返回null，建议用法
+        main();
+
+    }
+}
