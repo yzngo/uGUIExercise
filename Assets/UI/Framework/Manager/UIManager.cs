58c41678c5b0eaca227516ecb03688f18e92ec4a
Zhenning Yang <yzngo@live.com>
2019-12-25 12:15:54 +0800

    update

--- Assets/UI/Framework/Manager/UIManager.cs
+++ Assets/UI/Framework/Manager/UIManager.cs
@@ -20,22 +20,60 @@ public class UIManager
             return _instance;
         }
     }
-    private Transform _canvasTransform;
-    private Transform CanvasTransform
+    private Transform _desktopTransform;
+    private Transform DesktopTransform
     {
         get
         {
-            if (_canvasTransform == null)
+            if (_desktopTransform == null)
             {
-                _canvasTransform = GameObject.Find("Canvas").transform;
+                _desktopTransform = GameObject.Find("Desktop").transform;
             }
-            return _canvasTransform;
+            return _desktopTransform;
+        }
+    }
+    private Transform _windowTransform;
+    private Transform WindowTransform
+    {
+        get
+        {
+            if (_windowTransform == null)
+            {
+                _windowTransform = GameObject.Find("Window").transform;
+            }
+            return _windowTransform;
+        }
+    }
+    private Transform _dialogTranform;
+    private Transform DialogTranform
+    {
+        get
+        {
+            if (_dialogTranform == null)
+            {
+                _dialogTranform = GameObject.Find("Dialog").transform;
+            }
+            return _dialogTranform;
+        }
+    }
+    private Transform _topmostTransform;
+    private Transform TopmostTransform
+    {
+        get
+        {
+            if (_topmostTransform == null)
+            {
+                _topmostTransform = GameObject.Find("Topmost").transform;
+            }
+            return _topmostTransform;
         }
     }
     private Dictionary<string, GameObject> pageDict;
 
     private UIManager()
     {
+        pageDict = new Dictionary<string, GameObject>();
+        GameObject.DontDestroyOnLoad(DesktopTransform.gameObject);
         ParseUIPageListJson();
     }
 
@@ -46,10 +84,24 @@ public class UIManager
 
     public void OpenWindow(GameObject page)
     {
+        page.transform.SetParent(WindowTransform);
+        page.transform.SetAsFirstSibling();
         page.SetActive(true);
-        page.transform.SetAsLastSibling();
     }
 
+    public void OpenDialog(GameObject page)
+    {
+        page.transform.SetParent(DialogTranform);
+        page.transform.SetAsFirstSibling();
+        page.SetActive(true);
+    }
+
+    public void OpenTopmost(GameObject page)
+    {
+        page.transform.SetParent(TopmostTransform);
+        page.transform.SetAsFirstSibling();
+        page.SetActive(true);
+    }
     public void CloseWindow(GameObject page)
     {
         page.SetActive(false);
@@ -59,15 +111,16 @@ public class UIManager
     {
         foreach (KeyValuePair<string,GameObject> page in pageDict)
         {
-            var luaBehaviour = page.Value.GetComponent<LuaBehaviour>();
-            if(page.Value.activeSelf && luaBehaviour)
+            for(int i = 0; i < page.Value.GetComponentsInChildren<LuaBehaviour>(false).Length;i++)
             {
+                var luaBehaviour = page.Value.GetComponentsInChildren<LuaBehaviour>(false)[i];
                 var ret = luaBehaviour.SendMessage(id,param1,param2,param3);
                 if(ret == 1)
                 {
                     return;
                 }
             }
+
         }
     }
 
@@ -76,15 +129,9 @@ public class UIManager
     {
         TextAsset textPageList = Resources.Load<TextAsset>("page/UIPageList");
         UIPageList pageList = JsonUtility.FromJson<UIPageList>(textPageList.text);
-
-        if (pageDict == null)
-        {
-            pageDict = new Dictionary<string, GameObject>();
-        }
         foreach (UIPageInfo pageInfo in pageList.list)
         {
-            string path = pageInfo.path;
-            GameObject go = GameObject.Instantiate(Resources.Load<GameObject>(path), CanvasTransform, false);
+            GameObject go = GameObject.Instantiate(Resources.Load<GameObject>(pageInfo.path), WindowTransform, false);
             //BaumUI.Instantiate(gameObject, uiPrefab);
             pageDict.Add(pageInfo.name, go);
             go.SetActive(false);
